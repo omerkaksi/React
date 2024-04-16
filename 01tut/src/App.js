f6d609192f4ce7b2c21a2ad0b57385ea9233dcde
omerkaksi <omerkaksi@yahoo.com>
2024-04-16 12:41:11 +0300

    Lesson 3: React JS Functional Components; Header, Content, Footer.

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -1,33 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 function App() {
   const name="omer";
-  const handleNameChange = () =>{
-    const names = ['bob','ali','Hasan'];
-    const int =Math.floor(Math.random() *3 );
-    return names[int];
-  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <p>
-        Hello {name} {handleNameChange()}!!
-       </p>
-        <p>
-          Edit <code>src/App.js</code> and save to see changes..
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{name}[abc]</p>
-      </header>
+      <Header />
+      <Content/>
+      <Footer/>
      
     </div>
   );
---
+++ 01tut/src/Content.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Content = () => {
+    const handleNameChange = () =>{
+        const names = ['bob','ali','Hasan'];
+        const int =Math.floor(Math.random() *3 );
+        return names[int];
+      }
+  return (
+    <main>
+<p>
+    Hello {handleNameChange()}!
+</p>
+      
+
+    </main>
+  )
+}
+
+export default Content
