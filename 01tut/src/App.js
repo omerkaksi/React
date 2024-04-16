1ab83cca8e1a2f0b12d30bcbe61acb57cf4f6a2b
omerkaksi <omerkaksi@yahoo.com>
2024-04-16 10:41:41 +0300

    Lesson 2 : React App Component and JSX. JavaScript and XML

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name="omer";
+  const handleNameChange = () =>{
+    const names = ['bob','ali','Hasan'];
+    const int =Math.floor(Math.random() *3 );
+    return names[int];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+       <p>
+        Hello {name} {handleNameChange()}!!
+       </p>
         <p>
-          Edit <code>src/App.js</code> and save to see changes.
+          Edit <code>src/App.js</code> and save to see changes..
         </p>
         <a
           className="App-link"
@@ -17,7 +26,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>{name}[abc]</p>
       </header>
+     
     </div>
   );
 }
