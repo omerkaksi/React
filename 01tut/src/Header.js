02c0d20751ccc51cd09cd3922076a84c957e27a5
omerkaksi <omerkaksi@yahoo.com>
2024-04-17 10:23:25 +0300

    Lesson 4: CSS Styles in React JS

--- 01tut/src/Header.js
+++ 01tut/src/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const Header = () => {
+ /*  const headerStyle = {  
+    backgroundColor:'blue',
+  color:'#ff0'
+  }; */
+  //inline header header ın yanına style={headerStyle} ile
   return (
-    <header>
+    <header >
         <h1>Groceries List</h1>
         </header>
   )
 }
 
-export default Header
+export default Header;
