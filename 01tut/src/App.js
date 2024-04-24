f44f17e4fd667a205997f89cb36c40bf8d0c73fe
omerkaksi <omerkaksi@yahoo.com>
2024-04-24 11:38:12 +0300

    Lesson 13: Fetch Data from API in React

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -6,13 +6,33 @@ import Footer from './Footer';
 import { useState,useEffect } from 'react';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
-  const [newItem, setNewItem] = useState('')
-  const [search, setSearch] = useState('')
+  const API_URL='http://localhost:3500/items'
 
-  useEffect(() => { localStorage.setItem('shoppinglist', JSON.stringify(items));
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+  const [fetchError,setFetchError] = useState(null);
+  const [isLoading, setIsLoading]= useState(true);
 
-  },[items])
+  useEffect(() => { 
+    const fetchItems = async () => {
+      try{
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive expected data');
+        const listItems = await response.json();
+        console.log(listItems);
+        setItems(listItems);
+        setFetchError(null);
+      } catch(err){
+        setFetchError(err.message)
+      }finally{
+        setIsLoading(false);
+      }
+    }
+    setTimeout(()=> {fetchItems()},2000)
+    
+
+  },[])
 
 
 
@@ -52,11 +72,16 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <Content
+      <main>
+        {isLoading && <p>Loading Items</p>}
+        {fetchError && <p style ={{color: "red"}}>{`Error: ${fetchError}`}</p>}
+        
+      { !fetchError && !isLoading&& <Content
         items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
-      />
+      />}
+      </main>
       <Footer length={items.length} />
     </div>
   );
