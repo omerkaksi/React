77f18a18dd1b57ff9206afac98529bd3847db87f
omerkaksi <omerkaksi@yahoo.com>
2024-04-25 15:05:34 +0300

    Fetch API Data Project Part B.

--- 15tut/src/App.js
+++ 15tut/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Form from './Form';
-import List from './List'
+import List from './List';
+import Table from './Table';
 function App() {
   const API_URL = 'https://jsonplaceholder.typicode.com/';
   const [reqType, setReqType]=useState('users')
@@ -23,7 +24,8 @@ function App() {
   return (
     <div className="App">
       <Form reqType={reqType} setReqType={setReqType}/>
-      <List items={items} />
+      {/* <List items={items} /> */}
+      <Table items = {items}/>
     </div>
   );
 }
