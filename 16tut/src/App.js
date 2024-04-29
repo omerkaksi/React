b83f1fdbac62d1557a9932cc25ebacbf695cb483
omerkaksi <omerkaksi@yahoo.com>
2024-04-29 09:16:18 +0300

    Lesson 17 : React Rooter Hooks and Links. First part. Error.

--- 16tut/src/App.js
+++ 16tut/src/App.js
@@ -28,13 +28,13 @@ function App() {
       id: 3,
       title: "My 3rd Post",
       datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+      body: "Lorem iaspsum doaslor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
     },
     {
       id: 4,
       title: "My Fourth Post",
       datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+      body: "Lorexsm ipsum doadlor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
     }
   ])
   const [search, setSearch] = useState('');
